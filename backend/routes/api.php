d96bbcf5c2494024ef69eeced4356a3f2b69c285
sami valentina <samivalentinagarzon123@gmail.com>
2025-10-01 08:53:25 -0500

    solucion dos punto cuatro

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PerfileController;
 use App\Http\Controllers\usuariosController;
+use App\Http\Controllers\VehiculoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,13 @@ Route::post("/usuario/store", [usuariosController::class, "store"]);
 Route::get("/usuario/show/{id}", [usuariosController::class, "show"]);
 Route::post("/usuario/update/{id}", [usuariosController::class, "update"]);
 Route::post("/usuario/destroy/{id}", [usuariosController::class, "destroy"]);
+
+Route::get("/vehiculo/index", [VehiculoController::class, "index"]);
+Route::post("/vehiculo/store", [VehiculoController::class, "store"]); 
+Route::get("/vehiculo/show/{id}", [VehiculoController::class, "show"]);
+Route::post("/vehiculo/update/{id}", [VehiculoController::class, "update"]);
+Route::post("/vehiculo/destroy/{id}", [VehiculoController::class, "destroy"]);
+
 
 
 Route::get('/user', function (Request $request) {
