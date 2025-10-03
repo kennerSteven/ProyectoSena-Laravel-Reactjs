424562dcf86c7e4d47ee10eb256548ae6236bfd6
sami valentina <samivalentinagarzon123@gmail.com>
2025-10-03 11:30:17 -0500

    controlador vehiculos y usuarios

--- backend/app/Http/Controllers/UsuariosController.php
+++ backend/app/Http/Controllers/UsuariosController.php
@@ -2,27 +2,54 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\perfile;
+
 use App\Models\usuarios;
 use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
-    public function index()
-{
-    $usuarios = usuarios::with([
-        'perfile:id,nombre', 
-        'fichas:id,numeroFicha,nombrePrograma,jornada' 
-    ])->get();
+   public function index(){
 
-    return response()->json($usuarios);
-}
+   $usuario = usuarios::with('perfile','fichas')->get();
+   return response()->json($usuario);
 
-public function store(Request $request)
-{
-   
-   
-}
+   }
+
+   public function store(Request $request){
+
+    $usuario = usuarios::create($request->all());
+    return response()->json(['message' => 'Usuario creado correctamente',
+    'data' => $usuario], 201);
+   }
+
+
+   public function show(string $id){
+
+    $usuario = usuarios::with('perfile','fichas')->findOrFail($id);
+    return response()->json ($usuario,200);
+   }
+
+
+   public function update(Request $request, string $id ) {
+
+    $usuario = usuarios::findOrFail($id);
+
+    $usuario->update($request->all());
+
+    return response()->json(['message' => 'Usuario actualizado correctamente',
+    'data' => $usuario], 200);
+
+
+   }
+
+   public Function destroy(string $id) {
+
+    usuarios::findOrFail($id)->delete();
+    return response()->json('se elimino correctamente',200);
+
+   }
+
+
 
 
 }
--- backend/app/Http/Controllers/VehiculoController.php
+++ backend/app/Http/Controllers/VehiculoController.php
@@ -3,58 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\vehiculosRequest;
-use App\Models\usuarios;
 use App\Models\vehiculo;
 use Illuminate\Http\Request;
 
+
 class VehiculoController extends Controller
 {
-     public function index()
-    {
-        $vehiculos = vehiculo::with([
-          'usuarios:id,numeroDocumento'
-        ])->get();
-        return response()->json($vehiculos);
-    }
+   public function index(){
 
-   
-    public function store(Request $request)
-{
+    $vehiculos = vehiculo::with('usuarios')->get();
+    return response()->json($vehiculos, 200);
+
+   }
+
+   public function store(vehiculosRequest $request){
+
     $vehiculo = vehiculo::create($request->all());
 
-    $numeroDocumento = usuarios::where('id', $vehiculo->idusuario)->value('numeroDocumento');
-
-    return response()->json([
-        'vehiculo' => $vehiculo,
-        'numeroDocumentoUsuario' => $numeroDocumento
-    ], 201);
-}
-
-   
-
-     public function show($id)
-    {
-        $vehiculos = vehiculo::findOrFail($id);
-        $usuario =  usuarios::all();
-        
-        return response()->json([$vehiculos, $usuario]);
-    }
-
-    public function update(vehiculosRequest $request, $id)
-   {
-    $vehiculos = vehiculo::findOrFail($id);
-    $vehiculos->update($request->all());
-
-    return response()->json([ $vehiculos]);
+    return response()->json(['message' => 'Vehículo creado correctamente',
+    'data' => $vehiculo], 201);
    }
 
 
-   public function destroy($id)
-  {
-    $vehiculos = vehiculo::findOrFail($id);
-    $vehiculos->delete();
+   public function show(string $id){
 
-    return response()->json(['message' => 'vehiculo eliminado correctamente']);
-  }
+    $vehiculo = vehiculo::with('usuarios')->findOrFail($id);
+    return response()->json ($vehiculo,200);
+   }
+
+
+   public function update(vehiculosRequest $request, string $id ) {
+
+    $vehiculos = vehiculo::findOrFail($id);
+
+    $vehiculos->update($request->all());
+
+    return response()->json(['message' => 'Vehículo actualizado correctamente',
+    'data' => $vehiculos], 200);
+
+
+   }
+
+   public Function destroy(string $id) {
+
+    vehiculo::findOrFail($id)->delete();
+    return response()->json('se elimino correctamente',200);
+
+   }
+
 
 }
