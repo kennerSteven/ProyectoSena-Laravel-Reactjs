566178e3c2208a83d05f6eece862e3f8c4e6c68d
Kenner Steven <kennersteven4@gmail.com>
2025-10-24 07:43:58 -0500

    asdasd

--- backend/database/migrations/2025_10_02_144918_create_usuarios_table.php
+++ backend/database/migrations/2025_10_02_144918_create_usuarios_table.php
@@ -6,35 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+
             $table->string('nombre')->nullable();
             $table->string('apellido');
-            $table->enum('tipoDocumento',['ti','cc','otro'])->nullable();
+            $table->enum('tipoDocumento', ['ti', 'cc', 'otro'])->nullable();
             $table->string('numeroDocumento')->nullable();
             $table->string('telefono')->nullable();
-            $table->enum('tipoSangre',['A+','A-','B+','B-','AB+','AB-','O+','O-'])->nullable();
-             $table->enum('estado', ['activo', 'inactivo'])->default('activo');
+            $table->enum('tipoSangre', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])->nullable();
+            $table->enum('estado', ['activo', 'inactivo'])->default('activo');
+
             $table->timestamp('fechaRegistro')->useCurrent()->nullable();
             $table->date('fechaExpiracion')->nullable();
             $table->date('fechaFinContrato')->nullable();
             $table->string('foto')->nullable();
-            $table->unsignedBigInteger('idperfil')->nullable();
-            $table->foreign('idperfil')->references('id')->on('perfiles')->nullable();
+
+            // Clave foránea opcional: perfil
+            $table->unsignedBigInteger('idperfil');
+            $table->foreign('idperfil')->references('id')->on('perfiles');
+
+            // Clave foránea opcional: ficha
             $table->unsignedBigInteger('idficha')->nullable();
-            $table->foreign('idficha')->references('id')->on('fichas')->nullable();
+            $table->foreign('idficha')->references('id')->on('fichas');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('usuarios');
