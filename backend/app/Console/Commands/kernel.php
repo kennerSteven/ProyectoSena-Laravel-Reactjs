dfd8344e82652997f94653a8821c8e7caff5d6cd
sami valentina <samivalentinagarzon123@gmail.com>
2025-11-11 17:23:08 -0500

    disparador arreglo

--- backend/app/Console/Commands/kernel.php
+++ backend/app/Console/Commands/kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DesactivarContratosCommand;
 use App\Console\Commands\exVisitantescommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -13,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         exVisitantescommand::class, // tu comando
+        DesactivarContratosCommand::class,
     ];
 
     /**
