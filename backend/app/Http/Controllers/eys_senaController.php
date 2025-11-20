0a97175ff11717ad8ac3edfb49dbbdf13b56723f
kenner steven <kennersteven4@gmail.com>
2025-11-20 10:39:01 -0500

    Cambios

--- backend/app/Http/Controllers/eys_senaController.php
+++ backend/app/Http/Controllers/eys_senaController.php
@@ -16,21 +16,21 @@ class eys_senaController extends Controller
         return response()->json($registros);
     }
 
-   
+
     public function entradasena(Request $request)
     {
 
-        $request->validate(['numeroDocumento' => ['required', 'numeric', 'digits_between:6,15'], ]);
+        $request->validate(['numeroDocumento' => ['required', 'numeric', 'digits_between:6,15'],]);
 
         $usuario = usuarios::where('numeroDocumento', $request->numeroDocumento)->first();
 
         if ($usuario && $usuario->perfile->nombre === 'Visitante' && $usuario->estado === 'inactivo') {
-        $usuario->estado = 'activo';
-        $usuario->fechaExpiracion = null; 
-        $usuario->save();
-    }
+            $usuario->estado = 'activo';
+            $usuario->fechaExpiracion = null;
+            $usuario->save();
+        }
 
-        
+
         $entrada = eyssena::create([
             'numeroDocumento' => $usuario->numeroDocumento,
             'tipo' => 'entrada',
@@ -39,8 +39,8 @@ class eys_senaController extends Controller
         ]);
 
         $entrada->fechaRegistro = Carbon::parse($entrada->fechaRegistro)
-        ->timezone('America/Bogota')
-        ->format('Y-m-d H:i:s');
+            ->timezone('America/Bogota')
+            ->format('Y-m-d H:i:s');
 
 
         return response()->json([
@@ -50,22 +50,22 @@ class eys_senaController extends Controller
     }
 
 
-     public function salidasena(Request $request)
+    public function salidasena(Request $request)
     {
 
-       $request->validate(['numeroDocumento' => ['required', 'numeric', 'digits_between:6,15'],]);
+        $request->validate(['numeroDocumento' => ['required', 'numeric', 'digits_between:6,15'],]);
 
-       $usuario = usuarios::where('numeroDocumento', $request->numeroDocumento)->first();
+        $usuario = usuarios::where('numeroDocumento', $request->numeroDocumento)->first();
 
-       $ultimoRegistro = eyssena::where('idusuario', $usuario->id)->latest()->first();
+        $ultimoRegistro = eyssena::where('idusuario', $usuario->id)->latest()->first();
 
 
-    if (!$ultimoRegistro || $ultimoRegistro->tipo === 'salida') {
-    return response()->json([
-        'error' => ' No se puede registrar salida sin una entrada previa.'
-    ], 400);
-    }
-        
+        if (!$ultimoRegistro || $ultimoRegistro->tipo === 'salida') {
+            return response()->json([
+                'error' => ' No se puede registrar salida sin una entrada previa.'
+            ], 400);
+        }
+
 
         $salida = eyssena::create([
             'numeroDocumento' => $usuario->numeroDocumento,
@@ -73,166 +73,166 @@ class eys_senaController extends Controller
             'idusuario' => $usuario->id,
             'fechaRegistro' => now(),
         ]);
-    
 
-    if ($usuario->perfile->nombre === 'Visitante') {
-        $usuario->fechaExpiracion = now()->addHours(12);
-        $usuario->save();
-    }
 
-    $salida->fechaRegistro = Carbon::parse($salida->fechaRegistro)
-        ->timezone('America/Bogota')
-        ->format('Y-m-d H:i:s');
+        if ($usuario->perfile->nombre === 'Visitante') {
+            $usuario->fechaExpiracion = now()->addHours(12);
+            $usuario->save();
+        }
 
-    return response()->json([
-        'message' => 'Salida registrada correctamente',
-        'salida' => $salida
-    ]);
+        $salida->fechaRegistro = Carbon::parse($salida->fechaRegistro)
+            ->timezone('America/Bogota')
+            ->format('Y-m-d H:i:s');
 
-      
+        return response()->json([
+            'message' => 'Salida registrada correctamente',
+            'salida' => $salida
+        ]);
+
+
     }
 
 
     public function salidaMasivaSena()
-{
-   
-    $visitantesActivos = usuarios::where('estado', 'activo')
-        ->whereHas('perfile', function ($q) {
-            $q->where('nombre', 'Visitante');
-        })
-        ->get();
+    {
 
-    if ($visitantesActivos->isEmpty()) {
-        return response()->json(['message' => 'No hay visitantes activos para registrar salida.']);
-    }
+        $visitantesActivos = usuarios::where('estado', 'activo')
+            ->whereHas('perfile', function ($q) {
+                $q->where('nombre', 'Visitante');
+            })
+            ->get();
 
-    foreach ($visitantesActivos as $visitante) {
-        
-        \App\Models\eyssena::create([
-            'numeroDocumento' => $visitante->numeroDocumento,
-            'tipo' => 'salida',
-            'idusuario' => $visitante->id,
-            'fechaRegistro' => now(),
+        if ($visitantesActivos->isEmpty()) {
+            return response()->json(['message' => 'No hay visitantes activos para registrar salida.']);
+        }
+
+        foreach ($visitantesActivos as $visitante) {
+
+            eyssena::create([
+                'numeroDocumento' => $visitante->numeroDocumento,
+                'tipo' => 'salida',
+                'idusuario' => $visitante->id,
+                'fechaRegistro' => now(),
+            ]);
+
+
+            $visitante->fechaExpiracion = now()->addHours(12);
+            $visitante->save();
+        }
+
+        return response()->json([
+            'message' => 'Salidas registradas correctamente para todos los visitantes activos del SENA.',
+            'total' => $visitantesActivos->count()
         ]);
-
-       
-        $visitante->fechaExpiracion = now()->addHours(12);
-        $visitante->save();
     }
-
-    return response()->json([
-        'message' => 'Salidas registradas correctamente para todos los visitantes activos del SENA.',
-        'total' => $visitantesActivos->count()
-    ]);
-}
 
 
 
     public function buscarPorDocumento($numeroDocumento)
-{
-    $usuario = usuarios::with('perfile', 'fichas')
-        ->where('numeroDocumento', $numeroDocumento)
-        ->first();
+    {
+        $usuario = usuarios::with('perfile', 'fichas')
+            ->where('numeroDocumento', $numeroDocumento)
+            ->first();
 
-    if (!$usuario) {
-        return response()->json(['error' => 'Usuario no encontrado'], 404);
+        if (!$usuario) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
+        }
+
+        return response()->json([
+            'message' => 'Usuario encontrado',
+            'usuario' => $usuario
+        ]);
     }
 
-    return response()->json([
-        'message' => 'Usuario encontrado',
-        'usuario' => $usuario
-    ]);
-}
+
+    public function EstadisticasEntradasKPI()
+    {
+        $totalEntradas = eyssena::where('tipo', 'entrada')->count();
 
 
-public function EstadisticasEntradasKPI()
-{
-    $totalEntradas = eyssena::where('tipo', 'entrada')->count();
+        $entradasHoy = eyssena::where('tipo', 'entrada')
+            ->whereDate('fechaRegistro', now()->toDateString())
+            ->count();
+
+        $porcentaje = $totalEntradas > 0
+            ? round(($entradasHoy / $totalEntradas) * 100, 2)
+            : 0;
+
+        $porPerfil = eyssena::with('usuarios.perfile:id,nombre')
+            ->where('tipo', 'entrada')
+            ->get()
+            ->groupBy(function ($item) {
+
+                if ($item->usuarios && $item->usuarios->perfile) {
+                    return $item->usuarios->perfile->nombre;
+                }
+
+                return 'Visitante';
+            })
+            ->map(function ($grupo) {
+                return [
+                    'perfil' => $grupo->first()->usuarios->perfile->nombre
+                        ?? 'Visitante',
+                    'cantidad' => $grupo->count(),
+                ];
+            })
+            ->values();
 
 
-    $entradasHoy = eyssena::where('tipo', 'entrada')
-        ->whereDate('fechaRegistro', now()->toDateString())
-        ->count();
-
-    $porcentaje = $totalEntradas > 0
-        ? round(($entradasHoy / $totalEntradas) * 100, 2)
-        : 0;
-
-    $porPerfil = eyssena::with('usuarios.perfile:id,nombre')
-        ->where('tipo', 'entrada')
-        ->get()
-        ->groupBy(function ($item) {
-           
-            if ($item->usuarios && $item->usuarios->perfile) {
-                return $item->usuarios->perfile->nombre;
-            }
-            
-            return 'Visitante';
-        })
-        ->map(function ($grupo) {
-            return [
-                'perfil' => $grupo->first()->usuarios->perfile->nombre
-                    ?? 'Visitante',
-                'cantidad' => $grupo->count(),
-            ];
-        })
-        ->values();
-
-   
-    return response()->json([
-        'porcentaje' => $porcentaje,
-        'porperfil' => $porPerfil,
-        'total' => $totalEntradas,
-    ]);
-}
+        return response()->json([
+            'porcentaje' => $porcentaje,
+            'porperfil' => $porPerfil,
+            'total' => $totalEntradas,
+        ]);
+    }
 
 
-public function EstadisticasSalidasKPI()
-{
-  
-    $totalSalidas = eyssena::where('tipo', 'salida')->count();
+    public function EstadisticasSalidasKPI()
+    {
 
-    
-    $salidasHoy = eyssena::where('tipo', 'salida')
-        ->whereDate('fechaRegistro', now()->toDateString())
-        ->count();
-
-    
-    $porcentaje = $totalSalidas > 0
-        ? round(($salidasHoy / $totalSalidas) * 100, 2)
-        : 0;
-
-    
-    $porPerfil = eyssena::with('usuarios.perfile:id,nombre')
-        ->where('tipo', 'salida')
-        ->get()
-        ->groupBy(function ($item) {
-            if ($item->usuarios && $item->usuarios->perfile) {
-                return $item->usuarios->perfile->nombre;
-            }
-            return 'Visitante'; 
-        })
-        ->map(function ($grupo) {
-            return [
-                'perfil' => $grupo->first()->usuarios->perfile->nombre
-                    ?? 'Visitante',
-                'cantidad' => $grupo->count(),
-            ];
-        })
-        ->values();
+        $totalSalidas = eyssena::where('tipo', 'salida')->count();
 
 
-    return response()->json([
-        'porcentaje' => $porcentaje,
-        'porperfil' => $porPerfil,
-        'total' => $totalSalidas,
-    ]);
-}
+        $salidasHoy = eyssena::where('tipo', 'salida')
+            ->whereDate('fechaRegistro', now()->toDateString())
+            ->count();
+
+
+        $porcentaje = $totalSalidas > 0
+            ? round(($salidasHoy / $totalSalidas) * 100, 2)
+            : 0;
+
+
+        $porPerfil = eyssena::with('usuarios.perfile:id,nombre')
+            ->where('tipo', 'salida')
+            ->get()
+            ->groupBy(function ($item) {
+                if ($item->usuarios && $item->usuarios->perfile) {
+                    return $item->usuarios->perfile->nombre;
+                }
+                return 'Visitante';
+            })
+            ->map(function ($grupo) {
+                return [
+                    'perfil' => $grupo->first()->usuarios->perfile->nombre
+                        ?? 'Visitante',
+                    'cantidad' => $grupo->count(),
+                ];
+            })
+            ->values();
+
+
+        return response()->json([
+            'porcentaje' => $porcentaje,
+            'porperfil' => $porPerfil,
+            'total' => $totalSalidas,
+        ]);
+    }
 
 
 
 
 
-    
+
 
 }
